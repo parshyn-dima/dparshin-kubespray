9ab9cec794311d8947e7c986142670aab7524f29
Dmitriy Parshin <parshyn.dima@gmail.com>
2022-04-16 13:40:35 +0300

    add scale and upgrade script

---
+++ cluster_upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+cd terraform
+bash generate_inventory.sh > ../inventory/prod/test-k8s-calico/hosts.ini
+
+cd ../kubespray
+ansible-playbook -i ../inventory/prod/test-k8s-calico/hosts.ini --become --become-user=root upgrade-cluster.yml
